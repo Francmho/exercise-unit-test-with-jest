805051e21a972603e6057898b395ce966524f6dd
Franco Morales <73557750+Francmho@users.noreply.github.com>
2024-10-04 20:02:05 +0000

    Reviewed y corrected

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
+// This is my function that sums two numbers
 const sum = (a, b) => {
-    return a + b
+    return a + b;
 }
+// Just a console log for ourselves
 console.log(sum(7,3));
-
 
 
 
 // Declaramos una función con el nombre exacto "formEuroToDollar"
 //function fromEuroToDollar()
 
-    const fromEuroToDollar = function(valueInEuro){
-        // Convertimos el valor a dólares
-        let valueDollar = valueInEuro * 1.07;
-        // Retornamos el valor en dólares
-        return valueDollar;
-    }
+const fromEuroToDollar = function(valueInEuro){
+    // Convertimos el valor a dólares
+    let valueDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueDollar;
+}
 
 // Declaramos una función con el nombre exacto "formDollarToYen"
 
-    const formDollarToYen = function(valueInDolar){
-        // Convertimos el valor a Yen
-        let valueYen = valueInDolar * 158.10;
-        // Retornamos el valor en Yen
-        return valueYen;
-    }
+const fromDollarToYen = function(valueInDollar){
+    // Convertimos el valor a Yen
+    let valueYen = valueInDollar * 158.10;
+    // Retornamos el valor en Yen
+    return valueYen;
+}
 
 // Declaramos una función con el nombre exacto "formYenToPound"
 
-    const formYenToPound = function(valueInYen){
-        // Convertimos el valor a Pound
-        let valuePound = valueInYen * 0.0051;
-        // Retornamos el valor en Pounds
-        return valuePound;
-    }
+const fromYenToPound = function(valueInYen){
+    // Convertimos el valor a Pound
+    let valuePound = valueInYen * 0.0051;
+    // Retornamos el valor en Pounds
+    return valuePound;
+}
 
 
-    module.exports = {sum, fromEuroToDollar, formDollarToYen, formYenToPound};
+module.exports = {sum, fromEuroToDollar, fromDollarToYen, fromYenToPound};
 
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-// 1 Import the function sum from the app.js file
-const { sum, formDollarToYen } = require('./app.js');
+// Import the function sum from the app.js file
+const { expect, sum, fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
 
-// 2 Start your first test
+// 1 Start your first test
 test('adds 14 + 9 to equal 23', () => {
     // Inside the test we call our sum function with 2 numbers
     let total = sum(14, 9);
@@ -10,45 +10,41 @@ test('adds 14 + 9 to equal 23', () => {
      expect(total).toBe(23);
 });
 
-test("One euro should be 1.07 dollars", function() => {
+// 2. Test for Euro to Dollar conversion
+test("One euro should be 1.07 dollars", function() {
     //Import the function from app.js
-    const {fromEuroToDollar} = require('./app.js');
-    
+
     //Use the funcion like its supposed to be used
-    const {dollars} = fromEuroToDollar(3.5);
+    const dollars = fromEuroToDollar(3.5);
 
     //If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
-    const {expected} = 3.5 * 1.07;
+    const expected = 3.5 * 1.07;
 
     //This is the comparison for the unit test
-    expect(fromEuroToDollar(3.5)).toBe(3.745);// 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07) 
+    expect(dollars(3.5)).toBe(expected);// 3.5 euros should be 3.745
 });
 
-test("One dollar should be 158.10 Yen", function() => {
-    //Import the function from app.js
-    const {formDollarToYen} = require('./app.js');
+// 3. Test for Dollar to Yen conversion
+test("One dollar should be 158.10 Yen", function() {
 
     //Use the funcion like its supposed to be used
-    const {Yen} = fromEuroToYen(3.5);
+    const yen = fromDollarToYen(3.5);
 
     //If 1 Dollar is 158.10 Yen, then 3.5 euros should be (3.5 * 158.10)
-    const {expected} = 3.5 * 158.10;
+    const expected = 3.5 * 158.10;
 
     //This is the comparison for the unit test
-    expect(formDollarToYen(3.5)).toBe(553.35);// 1 dollar is 158.10 Yen, then 3.5 dollar should be = (3.5 * 158.10) 
+    expect(yen).toBe(expected);// 3.5 dollars should be 553.35
 });
 
-test("One Yen should be 0.0051 Pounds", function() => {
-    //Import the function from app.js
-    const {fromYentoPound} = require('./app.js');
-
+test("One Yen should be 0.0051 Pounds", function() {
     //Use the function like its supposed to be used
-    const {Pound} = fromYentoPound(3.5);
+    const pounds = fromYenToPound(3.5);
     
     //If one Yen is 0.0051 Pounds, then 3.5 Yen should be (3.5 * 0.0051)
-    const {expected} = 3.5 * 0.0051;
+    const expected = 3.5 * 0.0051;
 
     //This is the coparison for the unit test
-    expect(fromYentoPound(3.5)).toBe(0.01785); //1 Yen is 0.0051 Pound, then 3.5 Yen should be = (3.5 * 0.0051)
+    expect(pounds).toBe(expected); //1 Yen is 0.0051 Pound, then 3.5 Yen should be = (3.5 * 0.0051)
 
-)};
+});
